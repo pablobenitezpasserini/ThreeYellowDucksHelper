0a175cc7f6e531faea1b55ff61b22b08f80d348d
pablobenitezpasserini <pablobenitez252@gmail.com>
2025-11-12 20:06:10 -0300

    Fix margen botones, cards funcionles

---
+++ wwwroot/js/Index.js
@@ -0,0 +1,12 @@
+﻿let cardCostos = document.getElementById("cardCostos")
+let cardArticulo = document.getElementById("cardArticulo")
+
+cardCostos.addEventListener("click", () => redirectToProducts())
+cardArticulo.addEventListener("click", () => redirectToCreateCategory())
+function redirectToProducts() {
+    window.location.href = '/Productos/IndexProductos';
+}
+
+function redirectToCreateCategory() {
+    window.location.href = '/Categorias/CreateCategoria';
+}
